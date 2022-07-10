80731ef2167de85d0b7ffc33ba1b43aec88128c3
Fatty Fat prod <fattyfatprod@yandex.ru>
2022-07-10 17:47:23 +0300

    validate correction 3

--- script/index.js
+++ script/index.js
@@ -1,63 +1,5 @@
 
-//openPopup
-function openPopup(popup) {
-  popup.classList.add('popup_opend')
-
-  document.addEventListener('keyup', closeByEsc)
-  popup.addEventListener('click', closeByClick)
-}
-
-
-function closePopup(popup) {
-  popup.classList.remove('popup_opend')
-
-  document.removeEventListener('keyup', closeByEsc)
-  document.removeEventListener('click', closeByClick)
-}
-
-
-function closeByEsc(evt) {
-  if (evt.key === 'Escape') {
-    const openedPopup = document.querySelector('.popup_opend')
-    closePopup(openedPopup)
-  }
-}
-
-function closeByClick(evt) {
-  if (evt.target.classList.contains('popup_opend')) {
-    const openedPopup = document.querySelector('.popup_opend')
-    closePopup(openedPopup)
-  }
-}
-
-
-
-
-
-
-//profile
-const popupElementProfile = document.querySelector('.popup_type_edit-profile')
-
-const closeButtonProfile = document.querySelector('.popup__close-button')
-
-
-const formElementProfile = document.querySelector('.form')
-
-
-const nameInput = document.querySelector('.form__input_type_name')
-const jobInput = document.querySelector('.form__input_type_job')
-
-const saveButtonProfile = document.querySelector('.form__submit')
-
-
-const profile = document.querySelector('.profile')
-
-
-const profileName = document.querySelector('.profile__name')
-const profileJob = document.querySelector('.profile__job')
-const editButton = document.querySelector('.profile__edit-button')
-
-
+//Ссыки на карточки
 const initialCards = [
   {
     name: 'Архыз',
@@ -86,48 +28,25 @@ const initialCards = [
 ];
 
 
+//Находим форму, и модуьное окно
+const popupElementProfile = document.querySelector('.popup_type_edit-profile')
+
+const closeButtonProfile = popupElementProfile.querySelector('.popup__close-button')
 
 
-function submitEditProfileForm(evt) {
-  evt.preventDefault();
-
-  profileName.textContent = nameInput.value
-  profileJob.textContent = jobInput.value
-
-  closePopup(popupElementProfile)
-}
+const formElementProfile = popupElementProfile.querySelector('.form')
 
 
-popupElementProfile.addEventListener('submit', submitEditProfileForm)
-
-editButton.addEventListener('click', () => {
-  openPopup(popupElementProfile)
-
-  preValidation({
-    formSelector: '.form',
-    inputSelector: '.form__input',
-    submitButtonSelector: '.form__submit',
-    inactiveButtonClass: 'form__submit_disabled',
-    inputErrorClass: 'form__input_type_error',
-    errorClass: 'form__error_visible'
-  }, popupElementProfile)
-})
+const nameInput = formElementProfile.querySelector('.form__input_type_name')
+const jobInput = formElementProfile.querySelector('.form__input_type_job')
 
 
-closeButtonProfile.addEventListener('click', () => {
-  closePopup(popupElementProfile)
-})
+const profile = document.querySelector('.profile')
 
 
-
-
-
-
-
-
-
-//ElementCard
-
+const profileName = profile.querySelector('.profile__name')
+const profileJob = profile.querySelector('.profile__job')
+const editButton = profile.querySelector('.profile__edit-button')
 
 
 const popupElementCard = document.querySelector('.popup_type_add-card')
@@ -142,77 +61,8 @@ const cardNameInput = formElementCard.querySelector('.form__input_type_card-name
 const cardUrlInput = formElementCard.querySelector('.form__input_type_card-url')
 
 
-const addButton = document.querySelector('.profile__add-button')
+const addButton = profile.querySelector('.profile__add-button')
 
-function handleSubmitAddCardForm(evt) {
-  evt.preventDefault(); 
-  const newCard = {}
-  newCard.name = cardNameInput.value
-  newCard.link = cardUrlInput.value
-
-  
-  const newCardElement = createCard(newCard)
- 
-  addCard(newCardElement)
-
-
-  cardNameInput.value = ''
-  cardUrlInput.value = ''
-
-  closePopup(popupElementCard)
-}
-
-
-function submitAddCardForm(evt) {
-  evt.preventDefault();
-
-
-
-  const cardData = {
-    name: cardNameInput.value,
-    link: cardUrlInput.value
-  }
-
-  const newCardElement = createCard(newCard)
-
-  addCard(newCardElement)
-
-  cardNameInput.value = ''
-  cardUrlInput.value = ''
-
-
-
-
-  closePopup(popupElementCard)
-}
-
-addButton.addEventListener('click', () => {
-  openPopup(popupElementCard)
-
-  popupElementCard.addEventListener('submit', submitAddCardForm)
-
-
-  closeButtonCard.addEventListener('click', () => {
-    closePopup(popupElementCard)
-  })
-})
-
-
-
-
-
-
-
-
-
-//  FULLSCREEN
-const popupElementFullscreen = document.querySelector('.popup_type_fs')
-
-const closeButtonFullscreen = popupElementFullscreen.querySelector('.popup__close-button')
-
-closeButtonFullscreen.addEventListener('click', () => {
-  closePopup(popupElementFullscreen)
-})
 
 const figureElement = document.querySelector('.popup__fullscreen')
 
@@ -227,26 +77,64 @@ const galleryContainer = document.querySelector('.gallery')
 const cardTemplate = document.querySelector('#card-template').content
 
 
+const popupElementFullscreen = document.querySelector('.popup_type_fs')
+
+const closeButtonFullscreen = popupElementFullscreen.querySelector('.popup__close-button')
+
+const cardTemplateElement = cardTemplate.querySelector('.gallery__card')
+
+function handleSubmitEditProfileForm(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value
+  profileJob.textContent = jobInput.value
+
+
+  closePopup(popupElementProfile)
+}
+//Добавение карточки
+function handleSubmitAddCardForm(evt) {
+  evt.preventDefault();
+
+  const newCard = {}
+  newCard.name = cardNameInput.value
+  newCard.link = cardUrlInput.value
+
+
+  const newCardElement = createCard(newCard)
+
+  addCard(newCardElement)
+
+
+  //Пустые поля
+  cardNameInput.value = ''
+  cardUrlInput.value = ''
+
+
+  closePopup(popupElementCard)
+}
+
+
+//Добовяем карточку в начало 
 function createCard(element) {
-  const cardElement = cardTemplate.querySelector('.gallery__card').cloneNode(true)
+  const cardElement = cardTemplateElement.cloneNode(true)
   const cardImage = cardElement.querySelector('.gallery__image')
 
 
   cardImage.src = element.link
   cardImage.alt = element.name
 
-
   cardElement.querySelector('.gallery__text').textContent = element.name
 
-  const cardLike = cardElement.querySelector('.gallery__like')
-  cardLike.addEventListener('click', function (evt) {
-    evt.target.classList.toggle('gallery__like_active')
+  const likeButton = cardElement.querySelector('.gallery__like')
+
+  likeButton.addEventListener('click', function () {
+    likeButton.classList.toggle('gallery__like_active')
   })
 
 
   const cardDelete = cardElement.querySelector('.gallery__delete')
   cardDelete.addEventListener('click', (evt) => {
-    cardElement.remove()
+    cardDelete.parentElement.remove()
   })
 
 
@@ -265,30 +153,74 @@ function addCard(cardElement) {
   galleryContainer.prepend(cardElement)
 }
 
+//Закрытие попапа через overlay и Escape
+
+function openPopup(popup) {
+  popup.classList.add('popup_opend')
+
+  document.addEventListener('keyup', closeByEsc)
+  popup.addEventListener('click', closeByClick)
+}
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opend')
+  document.removeEventListener('keyup', closeByEsc)
+}
 
 
-initialCards.forEach(element => {
-  const newCard = createCard(element)
-  addCard(newCard)
+function closeByEsc(evt) {
+  if (evt.key === 'Escape') {
+    const openedPopup = document.querySelector('.popup_opend')
+    closePopup(openedPopup)
+  }
+}
+
+
+function closeByClick(evt) {
+  if (evt.target.classList.contains('popup_opend')) {
+    const openedPopup = document.querySelector('.popup_opend')
+    closePopup(openedPopup)
+  }
+}
+
+
+// Прикрепляем обработчик к кнопке сохранить в форме profile:
+
+popupElementProfile.addEventListener('submit', handleSubmitEditProfileForm)
+
+
+editButton.addEventListener('click', () => {
+  openPopup(popupElementProfile)
+})
+
+
+closeButtonProfile.addEventListener('click', () => {
+  closePopup(popupElementProfile)
 })
 
 
 popupElementCard.addEventListener('submit', handleSubmitAddCardForm)
 
-
+// Обработка кнопки Редактировать, открываем форму рекдактирования карточки
 addButton.addEventListener('click', () => {
   openPopup(popupElementCard)
 })
+
 
 closeButtonCard.addEventListener('click', () => {
   closePopup(popupElementCard)
 })
 
+// Обработка кнопки крестика, закрываем фуллскрин
 closeButtonFullscreen.addEventListener('click', () => {
   closePopup(popupElementFullscreen)
 })
 
-
+//  создаем карточки и добавляем их в галерею
+initialCards.forEach(element => {
+  const newCard = createCard(element)
+  addCard(newCard)
+})
 
 
 
